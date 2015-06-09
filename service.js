8adc48863f43eb0516f8e4424f53fa6b278a6177
Graham Jenson <grahamjenson@maori.geek.nz>
2015-06-09 11:10:10 +0300

    service A

---
+++ service.js
@@ -0,0 +1,12 @@
+var http = require('http');
+ 
+http.createServer(function (req, res) {
+  var greeting = "Hello"
+  http.get("http://localhost:8082", function(reply){
+   var who = ""
+   reply.on('data', function(data) { who += data })
+   reply.on('end', function() { 
+     res.end(greeting + " " + who) 
+    })
+  })
+}).listen(8081);
